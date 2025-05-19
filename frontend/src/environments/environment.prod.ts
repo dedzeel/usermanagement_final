2a39addf3958060e7cbd8101a85003dc154d9046
dedzeel <148021425+dedzeel@users.noreply.github.com>
2025-05-19 23:11:20 +0800

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://usermanagementfinal-production.up.railway.app'
+    apiUrl: 'https://usermanagementfinal-production-2381.up.railway.app'
 }
